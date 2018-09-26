2ecb1e8d0cd54e5f633f115c9c0f4a0ac93261e0
rhrv <36549075+rhrv@users.noreply.github.com>
2018-09-26 17:00:49 -0400

    Add files via upload
    Contains walmart & amazon api with price filters

--- Assign2/Walmart/2apiSearch.php
+++ Assign2/Walmart/2apiSearch.php
@@ -6,6 +6,7 @@ $version = '1.0.0';
 $appid = 'JonahDow-Books-PRD-793587284-2ba1979c'; 
 $globalid = 'EBAY-US';
 $query = $_GET['query']; 
+$numquery = $_GET['numValue'];
 $safequery = urlencode($query); 
 
 $apicall = "$endpoint?";
@@ -20,13 +21,15 @@ $resp = simplexml_load_file($apicall);
 
 if ($resp->ack == "Success") {
   $results = '';
-  foreach($resp->searchResult->item as $item) {
-    $pic   = $item->galleryURL;
-    $link  = $item->viewItemURL;
-    $title = $item->title;
-	$price = $item->sellingStatus->currentPrice;
-
-    $results .= "<tr><td><img src=\"$pic\"></td><td><a href=\"$link\">$title</a></td><tr><td>Most recent listing: $$price</tr></td>";
+  foreach($resp->searchResult->item as $item){
+	if($item->sellingStatus->currentPrice <= $numquery)
+	{
+		$pic   = $item->galleryURL;
+		$link  = $item->viewItemURL;
+		$title = $item->title;
+		$price = $item->sellingStatus->currentPrice;
+		$results .= "<tr><td><img src=\"$pic\"></td><td><a href=\"$link\">$title</a></td><tr><td>Most recent listing: $$price</tr></td>";
+	}
   }
 }
 
@@ -39,6 +42,7 @@ else {
   $string ='http://api.walmartlabs.com/v1/search?query=ipod&format=json&apiKey=nhqnfqjchruvskk5y67rfbmk';
   $endpoint ='http://api.walmartlabs.com/v1/search';
   $query = $_GET['query'];
+  $numquery2 = $_GET['numValue'];
   $format = 'json';
   $apiKey ='nhqnfqjchruvskk5y67rfbmk';
   $safequery=urlencode($query);
@@ -53,7 +57,13 @@ else {
 $resp = file_get_contents($apicall);
 //echo $resp;
 //Assigns the decoded JSON file to $items
- $items = json_decode ($resp, true);
+
+$items = json_decode ($resp, true);
+for($i = 0; $i < 10; $i++)
+{
+	if($items['items'][$i]['msrp'] > $numquery2)
+		unset($items['items'][$i]);
+}
 //Prints the queried item's name
 // print_r($items['items'][0]['name']);
 // echo '<br>';
@@ -86,6 +96,7 @@ $resp = file_get_contents($apicall);
 				<td>
 					<form method="GET">
 						<input type = "text" name = "query" value = "">
+						<input type = "number" name = "numValue" value = "">
 						<input type = "submit" value = "Search">
 					</form>
 				</td>
@@ -102,13 +113,16 @@ $resp = file_get_contents($apicall);
 			<p>
 				<b>Walmart results:</b>
 				<?php 
-				for($i = 0; $i <= 10; $i++)
+				for($i = 0; $i < 10; $i++)
 				{
-					echo '<br>';
-					print_r($items['items'][$i]['name']);
-					echo '<br>';
-					print_r("$" . $items['items'][$i]['msrp']);
-					echo '<br>';
+					if($items['items'][$i]['msrp'] > 0)
+					{
+						echo '<br>';
+						print_r($items['items'][$i]['name']);
+						echo '<br>';
+						print_r("$" . $items['items'][$i]['msrp']);
+						echo '<br>';
+					}
 				}
 				?>
 			</p>

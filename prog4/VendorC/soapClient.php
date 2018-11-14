95c07653cf4702af54b0f6971690fa0ecbed3878
Brandon - Main Laptop <bwserafin@gmail.com>
2018-11-13 23:58:14 -0500

    Brandon - Fixed my previous push

---
+++ prog4/VendorC/soapClient.php
@@ -0,0 +1,37 @@
+<?php
+require_once 'nusoap/lib/nusoap.php';
+$product = $_REQUEST['productName'];
+$quantity = $_REQUEST['productQuantity'];
+$maxPrice = $_REQUEST['productMaxPrice'];
+$client = new nusoap_client("http://localhost/COSC465/prog4/VendorC/soapServer.php");
+
+$searchData = array("category" => $product);
+$result = $client->call("getProd",$searchData);
+
+if (is_string($result)){
+  //echo "<h2>Fault</h2><pre>";
+  print_r($result);
+  echo "</pre>";
+
+  exit;
+}
+else{
+$error = $client->getError();
+  if($error){
+    echo"<h2>Error wwww</h2><pre>".$error. "</pre>";
+  }
+  else{
+    echo"<h2>Product</h2><pre>";
+    echo "</pre>";
+    echo " <br> Name: ". $result['title'];
+    echo "<br> Price: $". $result['price'];
+    echo " <br> ID:  ". $result['id'];
+    echo "<br> Quantity: ". $result['quantity'];
+  }
+}
+
+// echo "<hr /> <h2>Request</h2>";
+// echo "<pre>" . htmlspecialchars($client->request, ENT_QUOTES) . "</pre>";
+// echo "<h2>Response</h2>";
+// echo "<pre>" . htmlspecialchars($client->response, ENT_QUOTES) . "</pre>";
+ ?>
--- prog4/VendorC/soapServer.php
+++ prog4/VendorC/soapServer.php
@@ -1,12 +1,20 @@
 <?php
 require_once 'nusoap/lib/nusoap.php';
 
+/*
+*       Vendor C cheat sheet:
+*    Item         Quantity        Price
+     bananas ---- 200  ---------  $0.12
+     apples  ---- 200  ---------  $0.45
+     Mangos  ---- 50   ---------  $0.75
+     Pears   ---- 20   ---------  $0.50
+*/
 function getProd($category){
   if ($category == "bananas"){
     $data = array();
     $data['title'] = "bananas";
     $data['id'] = "4011";
-    $data['price'] = " 0.012";
+    $data['price'] = " 0.12";
     $data['quantity'] = "200";
 
     return $data;
@@ -15,7 +23,7 @@ function getProd($category){
     $data = array();
     $data['title'] = "apples";
     $data['id'] = "4017";
-    $data['price'] = " 0.45";
+    $data['price'] = " 0.35";
     $data['quantity'] = "200";
 
     return $data;
@@ -24,14 +32,22 @@ function getProd($category){
       $data = array();
       $data['title'] = "Mangos";
       $data['id'] = "5056";
-      $data['price'] = " 0.75";
+      $data['price'] = " 0.45";
       $data['quantity'] = "50";
 
       return $data;
     }
-  else {
+    elseif ($category == "pears"){
+      $data = array();
+      $data['title'] = "Pears";
+      $data['id'] = "5122";
+      $data['price'] = " 0.50";
+      $data['quantity'] = "20";
 
-    return "No Products at Vendor A... checking other vendors";
+      return $data;
+    }
+  else {
+    return "No Products at Vendor C. No Item Exists";
 
   }
 }

32d9909a307b35172e899869a85840014b864ec5
Brandon - Main Laptop <bwserafin@gmail.com>
2018-11-13 20:08:05 -0500

    Brandon - Added VendorC and added Price range and quantity to home page

--- prog4/VendorA/soapClient.php
+++ prog4/VendorA/soapClient.php
@@ -1,16 +1,21 @@
 <?php
 require_once 'nusoap/lib/nusoap.php';
 $product = $_REQUEST['productName'];
-$client = new nusoap_client("http://localhost/prog4/Vendora/SoapServer.php");
+$quantity = $_REQUEST['productQuantity'];
+$maxPrice = $_REQUEST['productMaxPrice'];
+$client = new nusoap_client("http://localhost/COSC465/prog4/VendorA/SoapServer.php");
 
-$searchData = array("category" => $product);
+$searchData = array("category" => $product,
+                    "quantity" => $quantity,
+                    "maxprice" => $maxPrice
+                    );
 $result = $client->call("getProd",$searchData);
 
 if (is_string($result)){
   //echo "<h2>Fault</h2><pre>";
   print_r($result);
   echo "</pre>";
-  
+
   exit;
 }
 else{

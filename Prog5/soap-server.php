461fd60b0f44447927ed723e8462337ff5dc926a
rhrv <36549075+rhrv@users.noreply.github.com>
2018-12-03 18:41:58 -0500

    Update soap-server.php
    Jonah - updated

--- Prog5/soap-server.php
+++ Prog5/soap-server.php
@@ -1,9 +1,12 @@
 <?php
 
+//require_once 'nusoap/lib/nusoap.php';
 require "eventServer.php";
 
 $server = new SoapServer('wsdl'); // wsdl file name
 $server->setClass('eventServer');
+//$server->register("getProd");
+//$server->register("getEventById");
 $server->handle();
 ?>
 
